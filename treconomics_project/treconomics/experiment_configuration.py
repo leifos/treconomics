adc5d7a4427e9357cdc4257e0f9ae4e8af80ea0d
Leif Azzopardi <leifos@acm.org>
2015-12-21 19:55:45 +0000

    loads 100index on pa

--- treconomics_project/treconomics/experiment_configuration.py
+++ treconomics_project/treconomics/experiment_configuration.py
@@ -14,9 +14,8 @@ work_dir = os.getcwd()
 
 
 my_whoosh_doc_index_dir = os.path.join(work_dir, 'data/fullindex/')
-if socket.gethostname() == 'leifos':
+if 'local' not in socket.gethostname():
     my_whoosh_doc_index_dir = '/home/leifos/test100index/'
-
 
 my_whoosh_query_index_dir = os.path.join(work_dir, "/trec_query_index/index")
 my_experiment_log_dir = work_dir
