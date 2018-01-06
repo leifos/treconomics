6a6da360a5b52ec576f1320b3df5d155866bbb67
David Maxwell <maxwelld90@gmail.com>
2018-01-06 14:10:19 +0000

    Added real user

--- treconomics_project/test_diversity_scoring.py
+++ treconomics_project/test_diversity_scoring.py
@@ -41,7 +41,14 @@ class TestScoring(unittest.TestCase):
         # judgements = [2, 2, None, None, 0]
         doc_lst = ['XIE20000424.0068', 'XIE20000424.0074', 'DUD', 'DUD2', 'XIE20000410.0142']
         results = get_performance_diversity(doc_lst, TOPIC_NUM)
-        self.assertAlmostEquals(results['trec_acc'], 0.666666)
+        self.assertAlmostEquals(results['trec_acc'], 2.0/3)
+    
+    def test_realuser(self):
+        # Topic 408
+        # judgements = [None, None, None, None, None, None, 2, 2]
+        doc_lst = ['APW20000706.0040', 'XIE19981024.0066', 'XIE19960728.0127', 'APW19991016.0181', 'APW19981005.1108', 'XIE19981106.0268', 'APW19990924.0040', 'APW19981019.0092']
+        results = get_performance_diversity(doc_lst, '408')
+        self.assertAlmostEquals(results['trec_acc'], 1.0)
 
 if __name__ == '__main__':
     unittest.main()
